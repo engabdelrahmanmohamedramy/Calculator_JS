2c836144f7ef913e5368c3bb417725735bbd384a
engabdelrahmanmohamedramy <eng.abdelrahman.mohamed.ramy45@gmail.com>
2020-11-21 16:27:27 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,90 @@
+var inptId = document.getElementById("myinput").value,
+    btn1 = document.getElementById("btn1").value,
+    btn2 = document.getElementById("btn2").value,
+    btn3 = document.getElementById("btn3").value,
+    btn4 = document.getElementById("btn4").value,
+    btn5 = document.getElementById("btn5").value,
+    btn6 = document.getElementById("btn6").value,
+    btn7 = document.getElementById("btn7").value,
+    btn8 = document.getElementById("btn8").value,
+    btn9 = document.getElementById("btn9").value,
+    btn10 = document.getElementById("btn10").value,
+    btn11 = document.getElementById("btn11").value,
+    btn12 = document.getElementById("btn12").value,
+    btn13 = document.getElementById("btn13").value,
+    btn14 = document.getElementById("btn14").value,
+    btn15 = document.getElementById("btn15").value,
+    btn16 = document.getElementById("btn16").value;
+      
+    
+function concatone(){
+  inptId += btn1;
+    document.getElementById("myinput").value = inptId;  
+}
+function concattwo(){
+     inptId += btn2;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatthree(){
+    inptId += btn3;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatplus(){
+     inptId += btn4;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatfour(){
+     inptId += btn5;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatfive(){
+     inptId += btn6;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatsix(){
+     inptId += btn7;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatminus(){
+     inptId += btn8;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatseven(){
+     inptId += btn9;
+    document.getElementById("myinput").value = inptId;  
+}
+function concategit(){
+     inptId += btn10;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatnine(){
+     inptId += btn11;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatmul(){
+     inptId += btn12;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatzero(){
+     inptId += btn13;
+    document.getElementById("myinput").value = inptId;  
+}
+function concatrest(){
+     inptId = "";
+    document.getElementById("myinput").value = inptId;  
+}
+function concatdiv(){
+     inptId += btn15;
+    document.getElementById("myinput").value = inptId;  
+}
+function concategual(){
+    document.getElementById("myinput").value = 
+ eval(inptId);
+}
+
+
+
+
+
+
+						
